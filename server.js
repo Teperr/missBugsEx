a0c41cff622b387f5593a35ede1cb8529b49dcf4
Teperr <omerteperr@gmail.com>
2024-06-24 03:12:51 +0300

    Add cookie-parser

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import {loggerService} from './services/logger.service'
+import cookieParser from 'cookie-parser'
 
 const app = express()
+app.use(cookieParser())
+
+app.use(express.static('public'))
+app.use(cookieParser())
 
 const port = 3030
 app.listen(port, () => loggerService.info(`Server listening on port http://127.0.0.1:${port}/`))//loggerService.info
